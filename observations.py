c45b00e849319d23f6727bf9cc2083d1f0af425d
Bosie <obosieakioyamen1@gmail.com>
2024-01-22 17:47:53 -0800

    moving data files under data_files directory.

--- observations.py
+++ observations.py
@@ -4,11 +4,11 @@ import pandas as pd
 # Get the current directory
 current_directory = os.getcwd()
 
-# Update the file paths with the correct relative paths
+# Update the file paths with the correct relative paths and the "data_files" directory
 file_paths = [
-    os.path.join(current_directory, "HomeA-meter2_2016.csv"),
-    os.path.join(current_directory, "HomeA-meter3_2016.csv"),
-    os.path.join(current_directory, "HomeA-meter4_2016.csv")
+    os.path.join(current_directory, "data_files", "HomeA-meter2_2016.csv"),
+    os.path.join(current_directory, "data_files", "HomeA-meter3_2016.csv"),
+    os.path.join(current_directory, "data_files", "HomeA-meter4_2016.csv")
 ]
 
 # Initialize a variable to store the total number of observations
